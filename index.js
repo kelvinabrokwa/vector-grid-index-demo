2990333ad4b373ba4f92cf661ff3a7c891ca1c4e
kelvinabrokwa <kelvinabrokwa@gmail.com>
2017-03-13 09:05:51 -0400

    alexandria

--- index.js
+++ index.js
@@ -1,35 +1,40 @@
 // global spatial index
 var loadedFeatures = {};
 var tree = rbush();
+var debugBoxes = [];
 
 var map;
+var buffer = .001;
+var infoDiv = document.getElementById('info');
 
 createMap();
 
 function createMap() {
-
   map = L.map('map', {});
 
-  //map.setView({lat: 43.6260475, lng: -70.295306}, 14);
-  map.setView({lat: 38.8027947337829, lng: -77.06497192382812}, 14);
+  map.setView({lat: 38.8027947337829, lng: -77.06497192382812}, 13);
 
   L.tileLayer('http://tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
 
-  //var url = 'http://tiles.vetro.io:4001/tiles/{z}/{x}/{y}.pbf';
-  var url = 'http://localhost:8080/tiles/{z}/{x}/{y}.pbf';
-  //var url = 'https://vectortiles.herokuapp.com/tiles/{z}/{x}/{y}.pbf';
+  var url = 'http://ec2-52-87-190-19.compute-1.amazonaws.com/tiles/{z}/{x}/{y}.pbf';
 
   var vectorTileOptions = {
     rendererFactory: L.canvas.tile,
     interactive: true,
-    view_businesses: [],
-    view_point_premise: [],
-    view_statewide_business: []
+    vectorTileLayerStyles: {
+      //parks: [],
+      //biketrails: [],
+      addresses: {
+        radius: 2
+      }
+    },
+    getFeatureId(feat) {
+      return feat.id;
+    }
   };
 
   var pbfLayer = L.vectorGrid.protobuf(url, vectorTileOptions)
     .on('click', function(e) {
-      var buffer = .001;
       var features = tree.search({
         minX: e.latlng.lng - buffer,
         minY: e.latlng.lat - buffer,
@@ -38,7 +43,31 @@ function createMap() {
       });
       console.log(features);
     })
+    .on('click', function(e) {
+      console.log(e);
+    })
     .addTo(map);
 
+  map.on('mousemove', function(e) {
+    var features = tree.search({
+      minX: e.latlng.lng - buffer,
+      minY: e.latlng.lat - buffer,
+      maxX: e.latlng.lng + buffer,
+      maxY: e.latlng.lat + buffer
+    });
+    infoDiv.innerHTML = features.map(f => '<div>' + f.layerName + ' ' + f.id + '</div>').join('');
+  });
 }
 
+function debug() {
+  var geojLayer = L.vectorGrid.slicer(turf.featureCollection(debugBoxes), {
+    rendererFactory: L.canvas.tile,
+    vectorTileLayerStyles: {
+      sliced: {
+        color: 'red'
+      }
+    }
+  })
+    .addTo(map);
+  console.log('debug on');
+}
